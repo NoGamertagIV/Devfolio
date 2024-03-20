9a64517027de4487bde4b609d624a5bf0c9face7
NoGamertagIV <148359018+NoGamertagIV@users.noreply.github.com>
2024-03-20 08:06:04 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@
  */
 
 "use strict";
+
+
+
+const  $themeBtn = document.querySelector("[data-theme-btn]");
+const  $HTML = document.documentElement;
+let isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
+
+if (sessionStorage.getItem('theme')) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
