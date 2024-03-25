523ed144c926fd4e23400c5fb4abf30afce2c923
Dylan <dylanbarga3@gmail.com>
2024-03-25 12:10:51 -0400

    last edits

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-/**
- * @copyright codewithsadee 2023
- * @author Sadee <codewithsadee24@gmail.com>
- */
-
-"use strict";
-
-
-
 const  $themeBtn = document.querySelector("[data-theme-btn]");
 const  $HTML = document.documentElement;
 let isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
