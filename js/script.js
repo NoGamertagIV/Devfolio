7234446092fc07813c261581e39d00e72d6c803c
Dylan <dylanbarga3@gmail.com>
2024-03-25 08:40:01 -0400

    tab functionality

--- js/script.js
+++ js/script.js
@@ -25,3 +25,23 @@ const changeTheme = () => {
 }
 
 $themeBtn.addEventListener("click", changeTheme);
+
+const $tabBtn = document.querySelector("[data-tab-btn]");
+let [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item => {
+    item.addEventListener("click", function () {
+
+       lastActiveTab.classList.remove("active");
+       lastActiveTabBtn.classList.remove("active");
+
+       const $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+       $tabContent.classList.add("active");
+       this.classList.add("active");
+
+
+       lastActiveTab = $tabContent;
+       lastActiveTabBtn = this;
+    });
+});
